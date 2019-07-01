860093d3c64e95fb3c974e205c65b5145181f870
paul <pdmxdd@gmail.com>
2019-07-01 01:07:28 -0500

    Class 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AstronautsComponent } from './astronauts/astronauts.component';
+import { CountriesComponent } from './countries/countries.component';
+import { MissionsComponent } from './missions/missions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AstronautsComponent,
+    CountriesComponent,
+    MissionsComponent
   ],
   imports: [
     BrowserModule
